31d84db5e5def5422411f717158cdbe93f22c8c9
d0k9 <vdokukin@bigmir.net>
2023-08-07 15:35:28 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,23 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': '**********',
 };
 
 function decode(expr) {
-    // write your solution here
+    const morseCodeArray = expr.match(/.{10}/g); 
+    let decodedString = ""; 
+
+    for (let i = 0; i < morseCodeArray.length; i++) {
+        const letter = morseCodeArray[i];
+        const morseLetter = letter.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-');
+        if (morseLetter === "**********") {
+            decodedString += " ";
+        } else {
+            decodedString += MORSE_TABLE[morseLetter];
+        }
+    }
+    return decodedString.trim(); 
 }
 
 module.exports = {
